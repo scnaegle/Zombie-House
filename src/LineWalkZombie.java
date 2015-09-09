c6472e16c3ae287db27b3c469838f13f5520af94
Sean Naegle <sean@naegles.com>
2015-09-08 23:38:37 -0600

    added extended classes for each of the zombie types

---
+++ src/LineWalkZombie.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sean on 9/8/15.
+ */
+public class LineWalkZombie extends Zombie
+{
+  public LineWalkZombie(Location location) {
+    super(location);
+  }
+}
---
+++ src/MasterZombie.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sean on 9/8/15.
+ */
+public class MasterZombie extends Zombie
+{
+  public MasterZombie(Location location) {
+    super(location);
+  }
+}
