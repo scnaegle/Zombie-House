10d1240e8ef0c9e0a60c1bc7285183d3c11e8d8b
Tyler Shelton <tshelt0n@unm.edu>
2015-09-08 23:36:00 -0600

    I Made a zombie and master zombie class. I am not quite sure how we are going to implement everything and how the program is going to talk to itself as of now, so I am just putting methods that I could see us needing before I actually start programming the zombies

---
+++ src/MasterZombie.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Tyler on 9/8/2015.
+ */
+
+//Perhaps we can make the master zombie move faster and see farther
+public class MasterZombie extends Zombie
+{
+
+}
---
+++ src/Zombie.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Tyler on 9/8/2015.
+ */
+
+/**
+ *
+ */
+public class Zombie
+{
+ // lookForLight();
+ // moveToPlayer();
+ // roamStraight lines();
+      // -for this method we will need to make sure that we are not trying to move
+      //  the zombies one tile at a time but in a continuios movement, i'm thinking we have them travel in a
+      // straight lines will be eaier than diagonol, but I could ver possibly be wrong
+      // maybe having them traverse for a certain amount of time in a gerneral
+}
