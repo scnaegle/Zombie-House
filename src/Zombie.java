f92cd63d7c082d4fd11eadeae86c79ddfd2436e7
Ally <star.goodman@gmail.com>
2015-09-09 15:41:41 -0600

    deleted unnecessary stuff

--- src/Zombie.java
+++ src/Zombie.java
@@ -6,9 +6,8 @@ public abstract class Zombie extends GameObject implements Humanoid
   protected double decision_rate = 2.0;
   protected double smell = 7.0;
   protected Heading heading = Heading.STILL;
-  protected Location location;
-  double defaultSpeed = .5;  // These need to be able to be changed easily
-  protected double speed = defaultSpeed;
+ 
+  protected double speed = .5;
 
   public Zombie(Location location) {
     this.location = location;
