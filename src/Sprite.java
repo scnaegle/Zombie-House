a205da482ceb937d0bda219669db9540976612aa
Sean Naegle <sean@naegles.com>
2015-09-09 18:13:57 -0600

    fixed merge conflicts

--- src/Sprite.java
+++ src/Sprite.java
@@ -7,9 +7,8 @@ import java.io.IOException;
  */
 public class Sprite
 {
-
+  private static BufferedImage sprite_sheet;
   private static final int TILE_SIZE = 80;
-  private static BufferedImage spriteSheet;
 
   public static BufferedImage loadSprite(String file)
   {
@@ -18,8 +17,7 @@ public class Sprite
 
     try
     {
-      sprite =
-          ImageIO.read(Sprite.class.getResource("resources/" + file + ".png"));
+      sprite = ImageIO.read(Sprite.class.getResource("resources/" + file + ".png"));
     }
     catch (IOException e)
     {
@@ -29,17 +27,16 @@ public class Sprite
     return sprite;
   }
 
-  public static BufferedImage getSprite(String sprite, int row, int col)
-  {
+  public static BufferedImage getSprite(String sprite, int row, int col) {
 
-    if (spriteSheet == null)
-    {
-      spriteSheet = loadSprite(sprite);
+    if (sprite_sheet == null) {
+      sprite_sheet = loadSprite(sprite);
     }
 
-    return spriteSheet.getSubimage((col * TILE_SIZE) - TILE_SIZE,
-        (row * TILE_SIZE) - TILE_SIZE,
-        TILE_SIZE, TILE_SIZE);
+    // Set back the row and col so that the index starts at 1
+    col--;
+    row--;
+    return sprite_sheet.getSubimage(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
 
 }
