59867d6065febcaa15e9c46bfe470661ac25b190
Sean Naegle <sean@naegles.com>
2015-09-18 13:57:03 -0600

    reorganize and fix creating tile map from a block grid

--- src/GameMap.java
+++ src/GameMap.java
@@ -62,7 +62,6 @@ public class GameMap
 
   private static Random random = new Random();
   private static Block[][] blockGrid = new Block[Y_SIZE][X_SIZE];
-  Tile tileGrid[][] = new Tile[Y_SIZE][X_SIZE];
 
   private static final boolean SHOW_COORDS = false;
 
@@ -73,6 +72,75 @@ public class GameMap
   private Tile[][] grid;
 
   private ArrayList<Tile> walls = new ArrayList<>();
+
+  private Location start_location;
+
+
+  public GameMap()
+  {
+    generateMap();
+    int r = 0;
+    this.num_rows = Y_SIZE;
+    this.num_cols = X_SIZE;
+    grid = new Tile[num_rows][num_cols];
+    Random rand = new Random();
+    for (Block[] row : blockGrid)
+    {
+      int c = 0;
+      for (Block block : row)
+      {
+        Tile new_tile = new Tile(block.y, block.x, block.type);
+        grid[r][c] = new_tile;
+        if (new_tile.tile_type == TileType.START) {
+          start_location = new_tile.location;
+        }
+        if (new_tile.tile_type == TileType.WALL)
+        {
+          walls.add(new_tile);
+        }
+        if (new_tile.tile_type == TileType.BRICK)
+        {
+          if (rand.nextDouble() < GUI.zspawn)
+          {
+            Zombie zombie;
+            Location location =
+                new Location(c * GUI.tile_size, r * GUI.tile_size);
+            if (rand.nextBoolean())
+            {
+              zombie = new RandomWalkZombie(GUI.zspeed, GUI.zsmell, GUI.drate,
+                  location);
+            }
+            else
+            {
+              zombie = new LineWalkZombie(GUI.zspeed, GUI.zsmell, GUI.drate,
+                  location);
+            }
+            zombies.add(zombie);
+          }
+
+          if (rand.nextDouble() < GUI.fspawn)
+          {
+            FireTrap fireTrap;
+            Location location =
+                new Location(c * GUI.tile_size, r * GUI.tile_size);
+
+            fireTrap = new FireTrap(location);
+            traps.add(fireTrap);
+
+          }
+        }
+        c++;
+      }
+      r++;
+    }
+    System.out.println("map: ");
+    System.out.println(toString());
+  }
+
+  public GameMap(File file)
+  {
+    createFromFile(file);
+  }
 
 
 
@@ -138,17 +206,6 @@ public class GameMap
     turnDoorToFloor(); // makes door to floor
     //makeInteriorWalls();
 
-    //makes the tile map
-    System.out.println("help");
-    Tile tileGrid[][] = new Tile[Y_SIZE][X_SIZE];
-    for (int y = 0; y < Y_SIZE; y++)
-    {
-      for (int x = 0; x < X_SIZE; x++)
-      {
-        System.out.println(x);
-        tileGrid[y][x] = getBlock(x, y).toTile();
-      }
-    }
     System.out.println("help1");
     for (int y = 0; y < Y_SIZE; y++)
     {
@@ -1257,66 +1314,6 @@ public class GameMap
   }
 
 
-  public GameMap()
-  {
-    generateMap();
-    int r = 0;
-    Random rand = new Random();
-    for (Tile[] row : tileGrid)
-    {
-
-      int c = 0;
-      for (Tile col : row)
-      {
-        grid[r][c] = col;
-        if (col.tile_type == TileType.WALL)
-        {
-          walls.add(col);
-        }
-        if (col.tile_type == TileType.BRICK)
-        {
-          if (rand.nextDouble() < GUI.zspawn)
-          {
-            Zombie zombie;
-            Location location =
-                new Location(c * GUI.tile_size, r * GUI.tile_size);
-            if (rand.nextBoolean())
-            {
-              zombie = new RandomWalkZombie(GUI.zspeed, GUI.zsmell, GUI.drate,
-                  location);
-            }
-            else
-            {
-              zombie = new LineWalkZombie(GUI.zspeed, GUI.zsmell, GUI.drate,
-                  location);
-            }
-            zombies.add(zombie);
-          }
-
-          if (rand.nextDouble() < GUI.fspawn)
-          {
-            FireTrap fireTrap;
-            Location location =
-                new Location(c * GUI.tile_size, r * GUI.tile_size);
-
-            fireTrap = new FireTrap(location);
-            traps.add(fireTrap);
-
-          }
-        }
-        c++;
-      }
-      r++;
-    }
-  }
-//
-//  public GameMap(File file)
-//  {
-//    createFromFile(file);
-//  }
-//
-//
-
   public int getWidth(int tile_size)
   {
     return num_cols * tile_size;
@@ -1388,11 +1385,6 @@ public class GameMap
 
 
 
-  public static void main(String[] args)
-  {
-    generateMap();
-  }
-}
 //    GameMapWithBlocks gameMap = new GameMapWithBlocks();
 //    GameMap procedureGenerateMap = new GameMap(gameMap.generateMap());
 //    File map_file = null;
@@ -1418,110 +1410,115 @@ public class GameMap
 //    generateMap();
 //
 
-//
-//  /**
-//   * Creates map from a file
-//   *
-//   * @param file
-//   */
-//  private void createFromFile(File file)
-//  {
-//    Scanner sc;
-//    ArrayList<ArrayList<Tile>> grid = new ArrayList<ArrayList<Tile>>();
-//    Random rand = new Random();
-//    int row = 0;
-//    int col = 0;
-//
-//    try
-//    {
-//      sc = new Scanner(file);
-//      while (sc.hasNextLine())
-//      {
-//        ArrayList<Tile> row_array = new ArrayList<Tile>();
-//        String row_string = sc.nextLine();
-//        if (!row_string.startsWith("!"))
-//        {
-//          char[] row_types = row_string.toCharArray();
-//          col = 0;
-//
-//          for (char type : row_types)
-//          {
-//            Tile new_tile = new Tile(row, col, type);
-//            row_array.add(new_tile);
-//            if (new_tile.tile_type == TileType.WALL)
-//            {
-//              walls.add(new_tile);
-//            }
-//            if (new_tile.tile_type == TileType.BRICK) {
-//              if (rand.nextDouble() < GUI.zspawn)
-//              {
-//                Zombie zombie;
-//                Location location =
-//                    new Location(col * GUI.tile_size, row * GUI.tile_size);
-//                if (rand.nextBoolean())
-//                {
-//                  zombie =
-//                      new RandomWalkZombie(GUI.zspeed, GUI.zsmell, GUI.drate,
-//                          location);
-//                }
-//                else
-//                {
-//                  zombie = new LineWalkZombie(GUI.zspeed, GUI.zsmell, GUI
-// .drate,
-//                      location);
-//                }
-//                zombies.add(zombie);
-//              }
-//
-//              if (rand.nextDouble() < GUI.fspawn)
-//              {
-//                FireTrap fireTrap;
-//                Location location =
-//                    new Location(col * GUI.tile_size, row * GUI.tile_size);
-//
-//                fireTrap = new FireTrap(50, 50, location);
-//                traps.add(fireTrap);
-//
-//              }
-//            }
-//            col++;
-//          }
-//          grid.add(row_array);
-//          row++;
-//        }
-//      }
-//
-//      this.num_rows = grid.size();
-//      this.num_cols = grid.get(0).size();
-//      this.grid = new Tile[num_rows][num_cols];
-//      for (int i = 0; i < grid.size(); i++)
-//      {
-//        Tile[] row_array = new Tile[grid.get(i).size()];
-//        row_array = grid.get(i).toArray(row_array);
-//        this.grid[i] = row_array;
-//      }
-//    }
-//    catch (IOException e)
-//    {
-//      System.err.println("Can't find file: " + file.getName());
-//      System.exit(1);
-//    }
-//  }
-//
-//  @Override
-//  public String toString()
-//  {
-//    String ret = "GameMap{" +
-//        "num_rows=" + num_rows +
-//        ", num_cols=" + num_cols + "}\n";
-//    for (int row = 0; row < num_rows; row++)
-//    {
-//      for (int col = 0; col < num_cols; col++)
-//      {
-//        ret += grid[row][col].tile_type.grid_char;
-//      }
-//      ret += "\n";
-//    }
-//    return ret;
-//  }
-//}
+
+  /**
+   * Creates map from a file
+   *
+   * @param file
+   */
+  private void createFromFile(File file)
+  {
+    Scanner sc;
+    ArrayList<ArrayList<Tile>> grid = new ArrayList<ArrayList<Tile>>();
+    Random rand = new Random();
+    int row = 0;
+    int col = 0;
+
+    try
+    {
+      sc = new Scanner(file);
+      while (sc.hasNextLine())
+      {
+        ArrayList<Tile> row_array = new ArrayList<Tile>();
+        String row_string = sc.nextLine();
+        if (!row_string.startsWith("!"))
+        {
+          char[] row_types = row_string.toCharArray();
+          col = 0;
+
+          for (char type : row_types)
+          {
+            Tile new_tile = new Tile(row, col, type);
+            row_array.add(new_tile);
+            if (new_tile.tile_type == TileType.WALL)
+            {
+              walls.add(new_tile);
+            }
+            if (new_tile.tile_type == TileType.BRICK) {
+              if (rand.nextDouble() < GUI.zspawn)
+              {
+                Zombie zombie;
+                Location location =
+                    new Location(col * GUI.tile_size, row * GUI.tile_size);
+                if (rand.nextBoolean())
+                {
+                  zombie =
+                      new RandomWalkZombie(GUI.zspeed, GUI.zsmell, GUI.drate,
+                          location);
+                }
+                else
+                {
+                  zombie = new LineWalkZombie(GUI.zspeed, GUI.zsmell, GUI
+ .drate,
+                      location);
+                }
+                zombies.add(zombie);
+              }
+
+              if (rand.nextDouble() < GUI.fspawn)
+              {
+                FireTrap fireTrap;
+                Location location =
+                    new Location(col * GUI.tile_size, row * GUI.tile_size);
+
+                fireTrap = new FireTrap(50, 50, location);
+                traps.add(fireTrap);
+
+              }
+            }
+            col++;
+          }
+          grid.add(row_array);
+          row++;
+        }
+      }
+
+      this.num_rows = grid.size();
+      this.num_cols = grid.get(0).size();
+      this.grid = new Tile[num_rows][num_cols];
+      for (int i = 0; i < grid.size(); i++)
+      {
+        Tile[] row_array = new Tile[grid.get(i).size()];
+        row_array = grid.get(i).toArray(row_array);
+        this.grid[i] = row_array;
+      }
+    }
+    catch (IOException e)
+    {
+      System.err.println("Can't find file: " + file.getName());
+      System.exit(1);
+    }
+  }
+
+  @Override
+  public String toString()
+  {
+    String ret = "GameMap{" +
+        "num_rows=" + num_rows +
+        ", num_cols=" + num_cols + "}\n";
+    for (int row = 0; row < num_rows; row++)
+    {
+      for (int col = 0; col < num_cols; col++)
+      {
+        ret += grid[row][col].tile_type.grid_char;
+      }
+      ret += "\n";
+    }
+    return ret;
+  }
+
+  public static void main(String[] args)
+  {
+    generateMap();
+  }
+}
